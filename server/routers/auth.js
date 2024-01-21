8b30175fa61750566f4587060ffe328c4b8a479f
SethyRung <rungsethyhk@gmail.com>
2024-01-21 23:00:16 +0700

    implement authentication at back end

---
+++ server/routers/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+
+router.post("/login", authController.handleLogin);
+router.post("/register", authController.handleRegister);
+router.post("/currentUser", authController.currentUser);
+router.post("/refresh", authController.handleRefreshToken);
+router.post("/logout", authController.handleLogout);
+
+module.exports = router;
